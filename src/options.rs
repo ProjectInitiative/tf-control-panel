8557f55847ac38c17f3d6c717b422e8ce5d6340e
Andrew Gu <1711729+a-gu@users.noreply.github.com>
2021-07-10 15:43:49 -0700

    Formatting
    Run `rustfmt` to format "src/*" files.
    
    Create "[.editorconfig][1]" to reduce future formatting discrepancies
    between contributors, similar to the `rustfmt` changes above:
    
      - [Rust Style Guide][2] for "*.rs" Rust source code, also default
        styles for unmatched files.
      - [TOML][3] for "*.toml" files, with opinionated indentation and no
        trimming of trailing whitespace to account for multiline strings.
      - [markdownlint][4] for "*.md" files. I have a different preference
        for unordered list indentation that goes against rule
        "MD007/ul-indent", for better plaintext viewing with checkboxes from
        GitHub-flavored Markdown. Not an issue, since only formatting
        defaults, and no linter(s), were implemented.
    
    [1]: https://editorconfig.org "EditorConfig"
    [2]: https://github.com/rust-dev-tools/fmt-rfcs/blob/master/guide/guide.md#indentation-and-line-width "Rust Style Guide (Indentation and Line Width)"
    [3]: https://github.com/toml-lang/toml "TOML GitHub Repository"
    [4]: https://github.com/DavidAnson/markdownlint/blob/main/doc/Rules.md#md013---line-length "markdownlint (MD013)"

--- src/options.rs
+++ src/options.rs
@@ -1,27 +1,32 @@
 extern crate clap;
-use self::clap::{Arg, App};
+use self::clap::{App, Arg};
 
 // import constants from root import
 use crate::constants::*;
 
-pub fn get_matches() -> clap::ArgMatches<'static>
-{
+pub fn get_matches() -> clap::ArgMatches<'static> {
     return App::new(APP_NAME)
-                    .version(VERSION)
-                    .author(AUTHORS)
-                    .about(ABOUT_DESC)
-                    .arg(Arg::with_name(ARG_VERBOSE_NAME)
-                        .short(ARG_VERBOSE_SHORT)
-                        // .long(ARG_VERBOSE_LONG)
-                        .multiple(true)
-                        .help(ARG_VERBOSE_HELP))
-                    .arg(Arg::with_name(ARG_COLOR_NAME)
-                        .short(ARG_COLOR_SHORT)
-                        .long(ARG_COLOR_LONG)
-                        .help(ARG_COLOR_HELP))
-                    .arg(Arg::with_name("hid")
-                        .short("j")
-                        .long("hid")
-                        .help("print out HID USB info"))
-                    .get_matches();
+        .version(VERSION)
+        .author(AUTHORS)
+        .about(ABOUT_DESC)
+        .arg(
+            Arg::with_name(ARG_VERBOSE_NAME)
+                .short(ARG_VERBOSE_SHORT)
+                // .long(ARG_VERBOSE_LONG)
+                .multiple(true)
+                .help(ARG_VERBOSE_HELP),
+        )
+        .arg(
+            Arg::with_name(ARG_COLOR_NAME)
+                .short(ARG_COLOR_SHORT)
+                .long(ARG_COLOR_LONG)
+                .help(ARG_COLOR_HELP),
+        )
+        .arg(
+            Arg::with_name("hid")
+                .short("j")
+                .long("hid")
+                .help("print out HID USB info"),
+        )
+        .get_matches();
 }
